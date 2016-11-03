f47f16477e976d30a48c2baf128a6ed030ebb0dd
Monish Kumar Appusamy <monishappusamy@gmail.com>
2016-11-03 16:27:15 -0400

    Update RandomGeneratorLibrary.java

--- java/RandomGeneratorLibrary.java
+++ java/RandomGeneratorLibrary.java
@@ -1,5 +1,12 @@
 class RandomGeneratorLibrary{
-    public String generateReasonablyID() {
+    /**
+     * Use this method if you want a random number that doesn't repeat again + If 
+     * you are ok using timestamp to provide this functionality
+     * 
+     * @return Current timestamp as String
+     *
+     */
+    public String generateReasonablyRandomID() {
             // Using current Timestamp for producing random ID
             SimpleDateFormat simpleDateFormat =  new SimpleDateFormat("yyyyMMddhhmmss");
             String dateAsString = simpleDateFormat.format(new Date());
